6ee4b64ee3eb3373840ed8032b48a65cf3af98c9
chxxyx <codud0826@naver.com>
2022-12-18 13:41:59 +0900

    feat : Entity 생성

---
+++ src/main/java/com/chxxyx/dividendproject/persist/CompanyRepository.java
@@ -0,0 +1,10 @@
+package com.chxxyx.dividendproject.persist;
+
+import com.chxxyx.dividendproject.persist.entity.CompanyEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface CompanyRepository extends JpaRepository<CompanyEntity, Long> {
+
+}
---
+++ src/main/java/com/chxxyx/dividendproject/persist/DividendRepository.java
@@ -0,0 +1,10 @@
+package com.chxxyx.dividendproject.persist;
+
+import com.chxxyx.dividendproject.persist.entity.DividendEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface DividendRepository extends JpaRepository<DividendEntity, Long> {
+
+}
