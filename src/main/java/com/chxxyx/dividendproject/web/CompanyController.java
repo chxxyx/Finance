ca9ca535b9c81cbeb06946febc30b92aeec5a3e5
chxxyx <codud0826@naver.com>
2022-12-19 19:21:49 +0900

    feat : 회사 삭제 기능 구현

--- src/main/java/com/chxxyx/dividendproject/web/CompanyController.java
+++ src/main/java/com/chxxyx/dividendproject/web/CompanyController.java
@@ -1,10 +1,12 @@
 package com.chxxyx.dividendproject.web;
 
 import com.chxxyx.dividendproject.model.Company;
+import com.chxxyx.dividendproject.model.constants.CacheKey;
 import com.chxxyx.dividendproject.persist.entity.CompanyEntity;
 import com.chxxyx.dividendproject.service.CompanyService;
 import java.util.List;
 import lombok.AllArgsConstructor;
+import org.springframework.cache.CacheManager;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.http.ResponseEntity;
@@ -12,6 +14,7 @@ import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.util.ObjectUtils;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -23,6 +26,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class CompanyController {
 
 	private final CompanyService companyService;
+	private final CacheManager redisCacheManager;
 
 	@GetMapping("/company/autocomplete")
 	public ResponseEntity<?> autocomplete(@RequestParam String keyword) {
@@ -51,8 +55,16 @@ public class CompanyController {
 		return ResponseEntity.ok(company);
 	}
 
-	@DeleteMapping("/company")
-	public ResponseEntity<?> deleteCompany() {
-		return null;
+	@DeleteMapping("/company/{ticker}")
+	@PreAuthorize("hasRole('WRITE')")
+	public ResponseEntity<?> deleteCompany(@PathVariable String ticker) {
+		String companyName = this.companyService.deleteCompany(ticker);
+		this.clearFinanceCache(companyName);
+		return ResponseEntity.ok(companyName);
+	}
+
+	// 캐시 삭제
+	public void clearFinanceCache(String companyName) {
+		this.redisCacheManager.getCache(CacheKey.KEY_FINANCE).evict(companyName);
 	}
 }
