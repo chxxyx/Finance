13fc5b203813a453cac07bcbe63a02acb8ee14db
chxxyx <codud0826@naver.com>
2022-12-19 17:48:37 +0900

    feat : Controller 구현

--- src/main/java/com/chxxyx/dividendproject/service/MemberService.java
+++ src/main/java/com/chxxyx/dividendproject/service/MemberService.java
@@ -38,6 +38,12 @@ public class MemberService implements UserDetailsService {
 	}
 
 	public MemberEntity authenticate(Auth.SignIn member) {
-		return null;
+		var user = this.memberRepository.findByUsername(member.getUsername())
+			.orElseThrow(() -> new RuntimeException("존재 하지 않는 ID입니다."));
+
+		if (!this.passwordEncoder.matches(member.getPassword(), user.getPassword())) {
+			throw new RuntimeException("비밀번호가 일치하지 않습니다.");
+		}
+		return user;
 	}
 }
--- src/main/java/com/chxxyx/dividendproject/web/AuthController.java
+++ src/main/java/com/chxxyx/dividendproject/web/AuthController.java
@@ -1,5 +1,40 @@
 package com.chxxyx.dividendproject.web;
 
+import com.chxxyx.dividendproject.model.Auth;
+import com.chxxyx.dividendproject.security.TokenProvider;
+import com.chxxyx.dividendproject.service.MemberService;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@Slf4j
+@RestController
+@RequestMapping("/auth")
+@RequiredArgsConstructor
 public class AuthController {
 
+	private final MemberService memberService;
+	private final TokenProvider tokenProvider;
+
+	@PostMapping("/signup")
+	public ResponseEntity<?> signup(@RequestBody Auth.SignUp request) {
+		// 회원 가입 API
+
+		var result = this.memberService.register(request);
+
+		return ResponseEntity.ok(result);
+	}
+
+	@PostMapping("/signin")
+	public ResponseEntity<?> signin(@RequestBody Auth.SignIn request) {
+		// 로그인용 API
+		var member = this.memberService.authenticate(request);
+		var token = this.tokenProvider.generateToken(member.getUsername(), member.getRoles());
+
+		return ResponseEntity.ok(token);
+	}
 }
