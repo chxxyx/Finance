383cb13af75c8508e531cbc7832e70303015fdf7
chxxyx <codud0826@naver.com>
2022-12-19 17:40:18 +0900

    feat : Jwt 토큰 생성 메소드 구현

---
+++ src/main/java/com/chxxyx/dividendproject/security/TokenProvider.java
@@ -0,0 +1,66 @@
+package com.chxxyx.dividendproject.security;
+
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.ExpiredJwtException;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import java.util.Date;
+import java.util.List;
+import lombok.RequiredArgsConstructor;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+import org.springframework.util.StringUtils;
+
+@Component
+@RequiredArgsConstructor
+public class TokenProvider {
+
+	private static final String KEY_ROLES = "roles";
+	private static final long TOKEN_EXPIRE_TIME = 1000 * 60 * 60; // 1 시간
+
+	@Value("${spring.jwt.secret}")
+	private String secretKey;
+
+	/**
+	 * 토큰 생성 (발급)
+	 * @param username
+	 * @param roles
+	 * @return
+	 */
+	public String generateToken(String username, List<String> roles) {
+		Claims claims = Jwts.claims().setSubject(username);
+		claims.put(KEY_ROLES, roles);
+
+		var now = new Date();
+		var expiredDate = new Date(now.getTime() + TOKEN_EXPIRE_TIME);
+
+		return Jwts.builder().setClaims(claims)
+			.setIssuedAt(now) // 토큰 생성 시간
+			.setExpiration(expiredDate) // 토큰 만료 시간
+			.signWith(SignatureAlgorithm.HS512, this.secretKey) // 사용할 암호화 알고리즘, 비밀키
+			.compact();
+	}
+
+	private String getUsername(String token) {
+		return this.parseClaims(token).getSubject();
+	}
+
+	public boolean validToken(String token) {
+		if (!StringUtils.hasText(token)) {
+			return false;
+		}
+
+		var claims = this.parseClaims(token);
+		return !claims.getExpiration().before(new Date());
+	}
+
+	private Claims parseClaims(String token){
+		try {
+			return Jwts.parser().setSigningKey(this.secretKey)
+				.parseClaimsJws(token).getBody();
+		} catch (ExpiredJwtException e) {
+			return  e. getClaims();
+		}
+	}
+}
